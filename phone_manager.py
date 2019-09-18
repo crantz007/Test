7a3db959b8c6b74f668a76d8272076c29d5bb443
crantz <youskant@gmail.com>
2019-09-18 11:29:33 -0400

    cellphones

---
+++ phone_manager.py
@@ -0,0 +1,109 @@
+# Manage a list of phones
+# And a list of employees
+
+# Each employee gets 0 or 1 phones
+class Phone():
+
+    def __init__(self, id, make, model):
+        self.id = id
+        self.make = make
+        self.model = model
+        self.employee_id = None
+
+
+    def assign(self, employee_id):
+        self.employee_id = employee_id
+
+
+    def is_assigned(self):
+        return self.employee_id
+
+
+    def __str__(self):
+        return 'ID: {} Make: {} Model: {} Assigned to Employee ID: {}'.format(self.id, self.make, self.model, self.employee_id)
+
+
+
+class Employee():
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+    def __str__(self):
+        return 'ID: {} Name {}'.format(self.id, self.name)
+
+
+
+class PhoneAssignments():
+
+    def __init__(self):
+        self.phones = []
+        self.employees = []
+
+
+    def add_employee(self, employee):
+        # TODO raise exception if two employees with same ID are added
+        for e in self.employees:
+            if e.id == employee.id:
+                raise PhoneError('Employees with same id cannot be added')
+        self.employees.append(employee)
+
+
+    def add_phone(self, phone):
+        # TODO raise exception if two phones with same ID are added
+        for p in self.phones:
+            if p.id == phone.id:
+                raise PhoneError('Phones with same id cannot be added')
+        self.phones.append(phone)
+
+
+    def assign(self, phone_id, employee):
+        # Find phone in phones list
+        # TODO if phone is already assigned to an employee, do not change list, raise exception
+        # TODO if employee already has a phone, do not change list, and raise exception
+        # TODO if employee already has this phone, don't make any changes. This should NOT raise an exception.
+        for e in self.employees:
+            for phone in self.phones:
+                if phone.is_assigned() == e.id:
+                    raise Exception('Employee is already assigned to a phone')
+                if phone.id == phone_id:
+                    if phone.is_assigned() is None:
+                        phone.assign(employee.id)
+                        return phone
+                    elif phone.is_assigned() is not None:
+                        if phone.is_assigned() == employee.id:
+                            return phone
+                        else:
+                            raise Exception('This phone is already assigned to an employee')
+
+
+    def un_assign(self, phone_id):
+        # Find phone in list, set employee_id to None
+        for phone in self.phones:
+            if phone.id == phone_id:
+                phone.assign(None)   # Assign to None
+
+
+    def phone_info(self, employee):
+        # find phone for employee in phones list
+
+        # TODO  should return None if the employee does not have a phone
+        # TODO  the method should raise an exception if the employee does not exist
+        if employee not in self.employees:
+            raise PhoneError('Employee does not exist')
+
+        for phone in self.phones:
+            if phone.employee_id == employee.id:
+                return phone
+
+
+        return None
+
+
+class PhoneError(Exception):
+     pass
+
+
+
---
+++ test_phone_manager.py
@@ -0,0 +1,117 @@
+import unittest
+from phone_manager import Phone, Employee, PhoneAssignments, PhoneError
+
+class TestPhoneManager(unittest.TestCase):
+
+    def test_create_and_add_new_phone(self):
+
+        testPhone1 = Phone(1, 'Apple', 'iPhone 6')
+        testPhone2 = Phone(2, 'Apple', 'iPhone 5')
+
+        testPhones = [ testPhone1, testPhone2 ]
+
+        testAssignmentMgr = PhoneAssignments()
+        testAssignmentMgr.add_phone(testPhone1)
+        testAssignmentMgr.add_phone(testPhone2)
+
+        # assertCountEqual checks if two lists have the same items, in any order.
+        # (Despite what the name implies)
+        self.assertCountEqual(testPhones, testAssignmentMgr.phones)
+
+
+    def test_create_and_add_phone_with_duplicate_id(self):
+        # TODO add a phone, add another phone with the same id, and verify an PhoneError exception is thrown
+        # TODO you'll need to modify PhoneAssignments.add_phone() to make this test pass
+        testPhone1 = Phone(1, 'Apple', 'iPhone 6')
+        testPhone2 = Phone(1, 'Apple', 'iPhone 5')
+
+        testAssignmentMgr = PhoneAssignments()
+        testAssignmentMgr.add_phone(testPhone1)
+
+        with self.assertRaises(PhoneError):
+            testAssignmentMgr.add_phone(testPhone2)
+
+
+    def test_create_and_add_new_employee(self):
+        # TODO write this test and then remove the self.fail() statement
+        # Add some employees and verify they are present in the PhoneAssignments.employees list
+        employee1 = Employee(1, 'Qaalib')
+        employee2 = Employee(2, 'Snoop Dog')
+
+        testAssignmentmgr = PhoneAssignments()
+        testAssignmentmgr.add_employee(employee1)
+        testAssignmentmgr.add_employee(employee2)
+        self.assertIs(0, testAssignmentmgr.employees.index(employee1))
+        self.assertIs(1, testAssignmentmgr.employees.index(employee2))
+
+
+
+    def test_create_and_add_employee_with_duplicate_id(self):
+        # TODO write this test and then remove the self.fail() statement
+        # TODO you'll need to fix the add_employee method in PhoneAssignments to make this test PhoneAssignments
+        # This method will be similar to test_create_and_add_phone_with_duplicate_id
+        employee1 = Employee(1, 'Qaalib')
+        employee2 = Employee(1, 'Keanu Reeves')
+        testAssignmentMgr = PhoneAssignments()
+        testAssignmentMgr.add_employee(employee1)
+        with self.assertRaises(PhoneError):
+            testAssignmentMgr.add_employee(employee2)
+
+
+    def test_assign_phone_to_employee(self):
+        # TODO write this test and remove the self.fail() statement
+        # TODO you'll need to fix the assign method in PhoneAssignments
+        testEmployee = Employee(1, 'Qaalib')
+        testPhone = Phone(1, 'Apple', 'IPhone 10')
+        testPhone2 = Phone(2, 'Apple', 'IPhone X')
+        testManager = PhoneAssignments()
+        testManager.add_phone(testPhone2)
+        testManager.add_phone(testPhone)
+        testManager.add_employee(testEmployee)
+        self.assertEqual(testPhone, testManager.assign(testPhone.id, testEmployee))
+
+
+
+
+    def test_assign_phone_that_has_already_been_assigned_to_employee(self):
+        # If a phone is already assigned to an employee, it is an error to assign it to a different employee. A PhoneError should be raised.
+        # TODO write this test and remove the self.fail() statement
+        # TODO you'll need to fix the assign method in PhoneAssignments so it throws an exception if the phone is alreaady assigned.
+
+        testEmployee = Employee(1, 'Qaalib')
+        testEmployee2 = Employee(2, 'Snoopy')
+        testPhone = Phone(1, 'Apple', 'IPhone 10')
+        testManager = PhoneAssignments()
+        testManager.add_phone(testPhone)
+        testManager.add_employee(testEmployee)
+        testManager.add_employee(testEmployee2)
+        testManager.assign(testPhone.id, testEmployee)
+        with self.assertRaises(Exception):
+            testManager.assign(testPhone.id, testEmployee2)
+
+
+    def test_assign_phone_to_employee_who_already_has_a_phone(self):
+        # TODO write this test and remove the self.fail() statement
+        # TODO you'll need to fix the assign method in PhoneAssignments so it raises a PhoneError if the phone is alreaady assigned.
+
+        testEmployee = Employee(1, 'Qaalib')
+        testPhone = Phone(1, 'Apple', 'IPhone 10')
+        testPhone2 = Phone(2, 'Apple', 'IPhone X')
+        testManager = PhoneAssignments()
+        testManager.add_phone(testPhone)
+        testManager.add_phone(testPhone2)
+        testManager.add_employee(testEmployee)
+        testManager.assign(testPhone.id, testEmployee)
+        with self.assertRaises(Exception):
+            testManager.assign(testPhone2.id, testEmployee)
+
+
+    def test_assign_phone_to_the_employee_who_already_has_this_phone(self):
+        # TODO The method should not make any changes but NOT raise a PhoneError if a phone
+        # is assigned to the same user it is currenly assigned to.
+        testEmployee = Employee(1, 'Qaalib')
+        testPhone = Phone(1, 'Apple', 'IPhone 10')
+        testManager = PhoneAssignments()
+        testManager.add_phone(testPhone)
+        testManager.add_employee(testEmployee)
+        self.assertEqual(testPhone, testManager.assign(testPhone.id, testEmployee))
